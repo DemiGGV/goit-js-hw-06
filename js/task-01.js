f0dee4f5826d88475dabb57909a8fef84f5ad4f4
Heorhii Honcharov <0019@ukr.net>
2023-03-20 07:07:38 +0100

    begining

---
+++ js/task-01.js
@@ -0,0 +1,4 @@
+const listingEl = document.querySelector('#categories')
+console.log("🚀 ~ file: task-01.js:2 ~ listingEl:", listingEl)
+const liEl = listingEl.querySelector.childNodes('.item')
+console.log("🚀 ~ file: task-01.js:4 ~ liEl:", liEl)
